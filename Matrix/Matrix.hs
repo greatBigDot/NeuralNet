ae66ce6a396b9cfd99ab1a35c7e360b11df9663d
John Michael <figueroa.johnm@gmail.com>
2017-03-03 15:16:58 -0500

    Add error handling to matrix multiplication method (still just throws an exception); that should be it.

--- Matrix/Matrix.hs
+++ Matrix/Matrix.hs
@@ -21,7 +21,7 @@ transpose (v:vs) =
 
 isMatrix :: (Num a) => [[a]] -> Bool
 isMatrix mat = (all (\v -> length v == len) mat) && (not . null $ mat)
-  where len = if not . null $ mat then length . head $ mat else 0
+  where len = if not . null $ mat then length . head $ mat else (-1)
 
 add :: (Num a) => [a] -> [a] -> [a]
 add v1 v2 = if length v1 == length v2
@@ -55,7 +55,9 @@ column n mat = if ((0 <= n) && (n < (length(head(mat)))))
 --crossProd :: (Num a) => [a] -> [a] -> [a]
 
 mult :: (Num a) => [[a]] -> [[a]] -> [[a]]
-mult m1 m2 = funcToMat (\(x,y) -> dotProd (row x m1) (column y m2)) (length m1,length $ head m2)
+mult m1 m2 = if (length . head $ m1) == length m2
+               then funcToMat (\(x,y) -> dotProd (row x m1) (column y m2)) (length m1,length $ head m2)
+               else error "Matrix.mult: The width of matrix one is not equal to the height of matrix two."
 
 matToFunc :: [[a]] -> (Int,Int) -> a
 matToFunc m (x,y) = m !! x !! y
@@ -68,4 +70,3 @@ coMatrix :: (Int, Int) -> [[(Int,Int)]]
 coMatrix (0,_) = []
 coMatrix (_,0) = []
 coMatrix (x,y) = (coMatrix ((x-1),y)) ++ [map (\b -> (x-1,b)) [0..(y-1)]]
-
