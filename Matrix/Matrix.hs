75b2175b21d9d71fd7684e3160e0e0eecc87053e
greatBigDot <figueroa.johnm@gmail.com>
2017-04-24 04:45:27 +0100

    did a ton of stuff; compiles, but doesn't actually work

--- Matrix/Matrix.hs
+++ Matrix/Matrix.hs
@@ -1,7 +1,10 @@
-module Matrix where
+module Matrix.Matrix where
+
+type Vector a = [a]
+type Matrix a = [[a]]
 
 --zipWith, except the lists can't be of different lengths.
-dotZip :: (a -> b -> c) -> [a] -> [b] -> [c]
+dotZip :: (a -> b -> c) -> Vector a -> Vector b -> Vector c
 dotZip _ [] []         = []
 dotZip _ [] _          = error "Matrix.dotZip: Lists are of different lengths."
 dotZip _ _ []          = error "Matrix.dotZip: Lists are of different lengths."
@@ -10,76 +13,99 @@ dotZip f (x:xs) (y:ys) = (f x y):(dotZip f xs ys)
 (.$.) = flip dotZip
 
 --Matrix functions:
-height :: [[a]] -> Int
+height :: Matrix a -> Int
 height = length
 
-width :: [[a]] -> Int
+width :: Matrix a -> Int
 width = length . head
 
-dim :: [[a]] -> (Int, Int)
+dim :: Matrix a -> (Int, Int)
 dim mat = (height mat, width mat)
 
 --concat
 
-transpose :: [[a]] -> [[a]]
+transpose :: Matrix a -> Matrix a
 transpose mat = map (flip column mat) [0..(width mat)-1]
 
 --Since all is lazy, this returns true for []. I may want to change that later, but currently coMatrix treats [] is a matrix. Hopefully this won't be a source of bugs...
-isMatrix :: (Num a) => [[a]] -> Bool
+isMatrix :: (Num a) => Matrix a -> Bool
 isMatrix mat = (all (\v -> length v == len) mat)
   where len = length . head $ mat
 
-add :: (Num a) => [a] -> [a] -> [a]
+add :: (Num a) => Vector a -> Vector a -> Vector a
 add v1 v2 = if length v1 == length v2
               then dotZip (+) v1 v2
               else error "Matrix.add: Vectors are of different dimensions."
 
-sMult :: (Num a) => a -> [a] -> [a]
+sMult :: (Num a) => a -> Vector a -> Vector a
 sMult x ys = map (*x) ys
 
-dotProd :: (Num a) => [a] -> [a] -> a
+dotProd :: (Num a) => Vector a -> Vector a -> a
 dotProd v1 v2 = if length v1 == length v2
                   then sum (dotZip (*) v1 v2)
                   else error "Matrix.dotProd: Vectors are of different dimensions."
 
-toMatrix :: [a] -> [[a]]
+len :: (Floating a) => Vector a -> a
+len = sqrt . sum . map (^2)
+
+toMatrix :: Vector a -> Matrix a
 toMatrix v = [v]
 
-toMatrix' :: [a] -> [[a]]
+toMatrix' :: Vector a -> Matrix a
 toMatrix' v = map (:[]) v
 
-row :: Int -> [[a]] -> [a]
+row :: Int -> Matrix a -> Vector a
 row n mat = if 0 <= n && n < length mat
               then mat!!n
               else error "Matrix.row: Index is out of range."
 
-column :: Int -> [[a]] -> [a]
+column :: Int -> Matrix a -> Vector a
 column n mat = if 0 <= n && n < (length . head $ mat)
                  then map (\v -> v!!n) mat
                  else error "Matrix.column: Index is out of range."
 
---crossProd :: (Num a) => [a] -> [a] -> [a]
+--crossProd :: (Num a) => Vector a -> Vector a -> Vector a
 
-mult :: (Num a) => [[a]] -> [[a]] -> [[a]]
+mult :: (Num a) => Matrix a -> Matrix a -> Matrix a
 mult m1 m2 = if (length . head $ m1) == length m2
                then funcToMat (\(x,y) -> dotProd (row x m1) (column y m2)) (length m1,length $ head m2)
                else error "Matrix.mult: The width of matrix one is not equal to the height of matrix two."
 
-matToFunc :: [[a]] -> (Int,Int) -> a
+matToFunc :: Matrix a -> (Int,Int) -> a
 matToFunc m (x,y) = m !! x !! y
 
-funcToMat :: ((Int, Int) -> a) -> (Int,Int) -> [[a]]
-funcToMat f (x,y) = map (map f) (coMatrix (x,y))
+funcToMat :: ((Int, Int) -> a) -> (Int,Int) -> Matrix a
+funcToMat f (x,y) = map (map f) $ coMatrix (x,y)
+
+funcTo3DMat :: ((Int, Int, Int) -> a) -> (Int, Int, Int) -> [Matrix a]
+funcTo3DMat f (l,i,j) = map (map (map f)) $ co3DMatrix (l,i,j)
+
+co3DMatrix :: (Int, Int, Int) -> [Matrix (Int,Int,Int)]
+co3DMatrix (0,_,_) = []
+co3DMatrix (_,0,_) = []
+co3DMatrix (_,_,0) = []
+co3DMatrix (l,i,j) = co3DMatrix (l-1,i,j) ++ [map (map  (\(b,c) -> (l-1,b,c))) (coMatrix (i,j))]
+
+--map (map {:: Int -> Int -> (Int, Int, Int)}) {:: [[(Int,Int)]]}
+--map {:: } {:: [[(Int,Int)]]}
 
 --Produces a matrix that contains the value (x,y) at the coordinates (x,y). Length is x, height is y.
-coMatrix :: (Int, Int) -> [[(Int,Int)]]
+coMatrix :: (Int, Int) -> Matrix (Int,Int)
 coMatrix (0,_) = []
 coMatrix (_,0) = []
-coMatrix (x,y) = (coMatrix ((x-1),y)) ++ [map (\b -> (x-1,b)) [0..(y-1)]]
+coMatrix (x,y) = coMatrix (x-1,y) ++ [map (\b -> (x-1,b)) [0..(y-1)]]
 
---determinant :: (Num a) => [[a]] -> a
-elemMult :: (Num a) => [[a]] -> [[a]] -> [[a]]
+--determinant :: (Num a) => Matrix a -> a
+elemMult :: (Num a) => Matrix a -> Matrix a -> Matrix a
 elemMult = dotZip (dotZip (*))
 
-distribute :: (Num a) => (a -> a) -> [[a]] -> [[a]]
+distribute :: (Num a) => (a -> a) -> Matrix a -> Matrix a
 distribute = map . map
+
+fill :: a -> (Int, Int) -> Matrix a
+fill _ (0,_) = []
+fill _ (1,0) = [[]]
+fill x (1,1) = [[x]]
+fill x (1,j) = map (x:) (fill x (1,j-1))
+fill x (i,j) = fill x (1,j) ++ fill x (i-1,j)
+
--- NeuralNet/NeuralNet.hs
+++ NeuralNet/NeuralNet.hs
@@ -1,68 +1,80 @@
-import Matrix
+module NeuralNet.NeuralNet where
+
+import Matrix.Matrix hiding (Matrix)
 
 {-Preliminaries-}
 
 type Matrix = [[Double]]               --A matrix is a list of lists. See NeuralNet/Matrix/Matrix.hs for matrix and vector functions.
-type Data   = [([Double],Double)]      --An example data set is a list of pairs; the first element of each pair is the list of inputs, the second is the corresponding output.
+type Activator = Double -> Double      --An activation function refers here to a unary function over the reals; typically, this should "normalize" a neural signal. Example: a sigmoid function.
+data Net = Net [Matrix] Activator      --A Neural Network in this code is uniquely defined by (1) a list of weight matrices; and (2) An activation function. Note that more complex neural networks than those used here would require more; this is just a bare-bones net (at least, for now).
+type Data = (Matrix, Matrix)           --An example data set is a set of inputs paired with corresponding outputs. This will be used to refine a randomly generated net into a net that actually does what we want it to do. Hopefully.
 
-data Net = Net [Matrix] Data Double    --A Neural Network in this code is uniquely defined by (1) a list of weight matrices; (2) a set of training data; and (3) the parameter for the sigmoid activation function. Note that more complex neural networks than those used here would require much more; this is just a bare-bones net (at least, for now).
 
 --A 3D matrix containing all synaptic weights. The first parameter specifies the source layer, the second the source neuron, and the third the destination neuron.
 wts :: Net -> [Matrix]
-wts Net ws _ _ = ws
---The training data. Essentially, what the neural network should do is find a way to predict the outputs of some fucntion from inputs, given some example input-output pairs. Note that currently, there can be multiple inputs, but only one output; i.e., the function is from R^n to R. Later this may be updated to from R^n to R^m.
-dat :: Net -> Data
-dat Net _ dt _ = dt
---Determines the steepness of the sigmoid activation function; will usually just be 1.
-act :: Net -> Double
-act Net _ _  a = a
+wts (Net ws _) = ws
+
+--The activation function to normalize signals passing through the net. Typically maps (-inf,+inf) onto (-1,+1) or (0,1); for example, a sigmoid function or an arctangent function.
+act :: Net -> Activator
+act (Net _ a) = a
 
 --Given a layer, this outputs the 2D matrix containing the synaptic weights of that layer. The first parameter specifies the source neuron, and the second specifies the destination neuron. It is significantly easier to deal with these 2D matrices instead of the 3D matrix of all the weights (for example, note that the lists in the 3D matrix do not all have the same dimensions).
-weights :: Net -> (Int -> Matrix)
-weights Net ws _ _ = (\n -> ws!!n)
+weights :: Net -> Int -> Matrix
+weights (Net ws _) = (\n -> ws!!n)
 
---the number of layers in the neural network
-numLayers :: Net -> (Int)
-numLayers Net ws _ _ = length ws
+--The number of layers in a given neural network.
+numLayers :: Net -> Int
+numLayers (Net ws _) = length ws + 1
 
---the number of example input-output pairs
-numInputs :: Net -> (Int)
-numInputs Net _ dt _ = length dt
-
---the number of neurons in the given layer
+--The number of neurons in a given layer of a given neural network.
 numNeurons :: Net -> (Int -> Int)
-numNeurons Net ws _ _ = height . (weights ws) . fl
+numNeurons net = height . (weights net) . fl
   where fl x = if x==0 then x else x-1
 
---maps (-infinity, +infinity) to (0,1); "normalizes" the signal travelling through the network
-activate :: Net -> (Double -> Double)
-activate Net _ _ a = (\x -> e**(a*x) / (e**(a*x) + 1))
+--The number of sets of inputs in the given data set.
+numInputs :: Data -> Int
+numInputs = length . fst
+
+--Standard activation functions; given a real, this outputs a sigmoid function with that real as the scaling factor. Maps (-inf,+inf) onto (0,+1).
+stdAct :: Double -> (Double -> Double)
+stdAct a x = e**(a*x) / (e**(a*x) + 1)
+
+
+--Given an example data set (though really only the inputs are needed), a neural net, and a layer, this gives the neurons in that layer that are generated from the input. Layer 0 is just the input data; layer n is the activation function applied elementwise to the matrix product of the neurons in layer (n-1) and the weights in layer (n-1).
+neurons :: Data -> Net -> Int -> Matrix
+neurons (input,_) _ 0     = input
+neurons dat net lyr = (matrify . act $ net) ((neurons dat net (lyr-1) |*| weights net (lyr-1)))
+
+result :: Net -> [Double] -> Double
+result net input = unwrap $ neurons ([input],[]) net (numLayers net - 1)
+
+--unsafe
+unwrap :: Matrix -> Double
+unwrap [[x]] = x
+
+--A 3D matrix containing the values of all neurons. The first paramter specifies the layer, the second specifies the set of inputs, and the third specifies the particular neuron. I.e., neurons[5,3,1] represents the 1st node in the 5th layer originating from the 3rd input. This is just for completeness; as mentioned above, it's much easier to deal with (:: Int -> Matrix) than (:: [Matrix]).
+allNeurons :: Data -> Net -> [Matrix]
+allNeurons dat net = map (neurons dat net) [0..numLayers net - 1]
+
+
+----------------------------------------------TODO: ORGANIZE-------------------------------------
 
 
 {-Foundational Functionality-}
 
---Given a layer, this returns the 2D matrix containing the neuron values in that layer. The first parameter specifies which set of inputs it originated from, and the second specifies which neuron in the given layer is being referenced.
-neurons :: Net -> Int -> Matrix
-neurons net =
-  \l -> if l==0
-          then map fst $ dat net
-          else (map . map . activate net) . (foldr1 mult . [neurons, weights] <== net <== . subtract 1)
+--Measures how accurate the neural network is by comparing its output to a set of training data; other loss functions are possible, but this, a supervised learning paradigm, is easiest.
+trainLoss :: Data -> (Net -> Double)
+trainLoss dat net =
+  let targetOut = snd dat
+      netOut = neurons dat net (numLayers net - 1)
+      lftSqDf = dotZip . dotZip $ (\x y -> (x-y)^2)
+  in 0.5 * sum (map (\[q] -> q) (lftSqDf targetOut netOut))
 
---A 3D matrix containing the values of all neurons. The first paramter specifies the layer, the second specifies the set of inputs, and the third specifies the particular neuron. I.e., neurons[5,3,1] represents the 1st node in the 5th layer originating from the 3rd input. This is just for completeness; as mentioned above, it's much easier to deal with (:: Int -> Matrix) than (:: [Matrix]).
-allNeurons :: Net -> [Matrix]
-allNeurons net = map (neurons net) [0..numLayers - 1]
-
---Measures how accurate the neural network is by comparing its output to the training data.
-loss :: Net -> Double
-loss net = sum $ map (\k -> 1/2 * (diff (column k) [targetOut, actualOut]) ^ 2) [0..numInputs - 1]
-  where targetOut = transpose $ map snd $ dat net
-        actualOut = neurons net $ numLayers - 1
-        diff f xs = foldl (-) (map f xs)
 
 --grad :: (Num a) => (a,a,a) -> a
---grad (l,i,j) = sum $ map ((\k -> (column k out) - (column k targetOut)) [0..numInputs - 1]
+--grad (l,i,j) = sum $ map ((\k -> (column k out) - (column k targetOut)) [0..numInputs - 1] )
 
---gradient :: ( 
+--gradient :: 
 
 
 --Euler's constant
@@ -72,3 +84,80 @@ e = exp 1
 --kinda like a backwards map; instead of distributing a function over a list of inputs, it distributes an input over a list of functions (i.e., abs. {100-01})
 (<==) :: [a -> b] -> a -> [b]
 fs <== x = map ($x) fs
+
+
+
+
+--The derivative of the nth layer neural matrix with respect to weight (l,i,j).
+netDer :: Data -> Net -> (Int, Int, Int) -> Int -> Matrix
+netDer dat net (l,i,j) n
+  | n <= l    = fill 0 (dim $ neurons dat net n)
+  | n == l+1  = (neurons' |*| indicate (dim weights') (i,j)) |.| (act' (neurons' |*| weights'))
+  | otherwise = (netDer'  |*| weights')                      |.| (act' (neurons' |*| weights'))
+  where netDer'  = netDer dat net (l,i,j) (n-1)
+        neurons' = neurons dat net (n-1)
+        weights' = weights net (n-1)
+        act'     = lift1 . (derivative stdDel) . act $ net
+
+indicate :: (Int, Int) -> (Int, Int) -> Matrix
+indicate dimen (i,j) = lift1 (\(x,y) -> if (x,y) == (i,j) then 1 else 0) (coMatrix dimen)
+
+--The partial derivative of the loss function with respect to weight (l,i,j).
+grad :: Data -> Net -> (Int, Int, Int) -> Double
+grad dat net (l,i,j) = sum $ for [0..numInputs dat - 1] (\k -> (netOut |-| targetOut)!!k!!0 * (netDer dat net (l,i,j) (numLayers net - 1))!!k!!0)
+  where targetOut = snd dat
+        netOut    = neurons dat net (numLayers net - 1)
+
+gradient :: Data -> Net -> Int -> Matrix
+gradient dat net layer = funcToMat (\(i,j) -> grad dat net (layer,i,j)) (numNeurons net layer, numNeurons net (layer+1))
+
+for :: [a] -> (a -> b) -> [b]
+for = flip map
+
+lift1 :: (Functor t) => (a -> b) -> t (t a) -> t (t b)
+lift1 = fmap . fmap
+
+matrify = lift1
+
+--lift2 :: (Applicative t) => (a -> b -> c) -> t (t a) -> t (t b) -> t (t c)
+--lift2 = (<^>) . (<^>)
+
+--This just turns some binary function into a binary function over applicative types.
+(<^>) :: (Applicative t) => (a -> b -> c) -> (t a -> t b -> t c)
+(<^>) f = (<*>) . (f <$>)
+
+
+derivative :: Double -> (Double -> Double) -> (Double -> Double)
+derivative del f = \x -> (f (x + del) - f x) / del
+
+stdDel :: Double
+stdDel = 1.0e-12
+
+(|*|) :: Matrix -> Matrix -> Matrix
+(|*|) = mult
+
+(|.|) :: Matrix -> Matrix -> Matrix
+(|.|) =  dotZip . dotZip $ (*)
+
+(|+|) :: Matrix -> Matrix -> Matrix
+(|+|) = dotZip . dotZip $ (+)
+
+(|-|) :: Matrix -> Matrix -> Matrix
+(|-|) = dotZip . dotZip $ (-)
+
+
+step :: Double
+step = 0.01
+
+
+--Given a data set and a neural network, this creates a new neural network that (hopefully) preforms better. Calling this recursively should (hopefully) optimize the neural net.
+learn :: Data -> Net -> Net
+learn dat net =
+  let newWeights l = lift1 (*(-step)) (gradient dat net l) |+| weights net l
+      newWts       = map newWeights [0..numLayers net - 2]
+  in Net newWts (act net)
+
+--Applies the learning function to the starting net with the given data set the specified number of times.
+optimize :: Data -> Net -> Int -> Net
+optimize _   net 0 = net
+optimize dat net n = learn dat (optimize dat net (n-1))
